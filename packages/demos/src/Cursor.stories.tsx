177815a46f594479114d27d39d61288bf2caf43a
Jedrzej Wiewiora <jedrzej.wiewiora@gmail.com>
2023-01-25 09:36:39 +0100

    Cursor story

---
+++ packages/demos/src/Cursor.stories.tsx
@@ -0,0 +1,95 @@
+import React, { useRef } from "react";
+import { Meta, Story } from "@storybook/react";
+import { linePlotter, makeCursorPlugin } from "@canplot/core";
+import { usePlot } from "@canplot/react";
+
+export default {
+  title: "Cursor",
+} as Meta;
+
+const Template: Story = () => {
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  usePlot(
+    {
+      dimensions: { height: 400 },
+      canvasRef: ref,
+      plugins: [
+        makeCursorPlugin({
+          onHover: (event) => {
+            const tooltip = document.getElementById("tooltip")!;
+            tooltip.style.pointerEvents = "none";
+            tooltip.style.userSelect = "none";
+
+            tooltip.innerHTML = JSON.stringify(event.position, null, 2);
+            if (event.position) {
+              tooltip.style.display = "block";
+              tooltip.style.left = `${event.position.screen.x}px`;
+              tooltip.style.top = `${event.position.screen.y}px`;
+            } else {
+              tooltip.style.display = "none";
+            }
+          },
+          onClick: (event) => {
+            event.plot.update((old) => ({
+              ...old,
+              facets: [
+                ...(old.facets ?? []),
+                {
+                  type: "v-line",
+                  scaleId: "x-1",
+                  x: event.position.scaled["x-1"],
+                  style: { strokeStyle: "#ff000099" },
+                },
+              ],
+            }));
+          },
+          onDblClick: (event) => {
+            event.plot.update((old) => ({
+              ...old,
+              facets: [
+                ...(old.facets ?? []),
+                {
+                  type: "h-line",
+                  scaleId: "y-1",
+                  y: event.position.scaled["y-1"],
+                  style: { strokeStyle: "#00444499" },
+                },
+              ],
+            }));
+          },
+        }),
+      ],
+    },
+    () => {
+      return {
+        padding: 10,
+        scales: [{ id: "x-1" }, { id: "y-1" }, { id: "y-2" }],
+        axes: [{ scaleId: "x-1" }, { scaleId: "y-1" }, { scaleId: "y-2" }],
+        series: [
+          {
+            xScaleId: "x-1",
+            yScaleId: "y-1",
+            plotter: linePlotter(),
+            x: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            y: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+          },
+          {
+            xScaleId: "x-1",
+            yScaleId: "y-2",
+            plotter: linePlotter({
+              style: { strokeStyle: "red" },
+            }),
+            x: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            y: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+          },
+        ],
+      };
+    },
+    []
+  );
+
+  return <canvas ref={ref} />;
+};
+
+export const Default = Template.bind({});
