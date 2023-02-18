03700053da0492dae8e97fec95961160d71e4a42
Jedrzej Wiewiora <jedrzej.wiewiora@gmail.com>
2023-02-18 16:53:46 +0100

    Draw and plot demos

--- packages/demos/src/DrawAndPlot.stories.tsx
+++ packages/demos/src/DrawAndPlot.stories.tsx
@@ -1,77 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { linePlotter } from "@canplot/core";
-import { animationLoop, EmbeddedPlot } from "./helpers";
+import { linePlotter, makeCursorPlugin } from "@canplot/core";
+import { EmbeddedPlot } from "./helpers";
+import { usePlot } from "@canplot/react";
 
 export default {
   title: "Draw and Plot",
-  args: {
-    dimensions: { height: 400 },
-    plugins: [
-      {
-        hooks: {
-          onInit: ({ plot }) =>
-            animationLoop(() => {
-              plot.update((plot) => {
-                plot.series[0].y = new Array(plot.series[0].x.length)
-                  .fill(0)
-                  .map(
-                    (_, y) => 5 + Math.sin(y / 10 + performance.now() / 100)
-                  );
-                plot.series[1].y = new Array(plot.series[1].x.length)
-                  .fill(0)
-                  .map(
-                    (_, y) => 2 + Math.cos(y / 10 + performance.now() / 100)
-                  );
-                return plot;
-              });
-            }),
-        },
-      },
-    ],
-
-    padding: 10,
-    axes: [
-      { scaleId: "x-1" },
-      { scaleId: "x-1", position: "secondary" },
-      { scaleId: "x-1", position: "secondary" },
-      {
-        scaleId: "y-1",
-        style: { fillStyle: "red", strokeStyle: "red" },
-      },
-      {
-        scaleId: "y-1",
-        style: { fillStyle: "red", strokeStyle: "red" },
-        tickSize: 20,
-      },
-      { scaleId: "y-1" },
-      { scaleId: "y-1", position: "secondary" },
-      { scaleId: "y-1", position: "secondary" },
-      { scaleId: "y-1", position: "secondary" },
-      { scaleId: "y-1", position: "secondary" },
-    ],
-    scales: [{ id: "x-1" }, { id: "y-1" }],
-    series: [
-      {
-        xScaleId: "x-1",
-        yScaleId: "y-1",
-        plotter: linePlotter({ style: { strokeStyle: "blue" } }),
-        x: new Array(1000).fill(0).map((_, i) => i / 10),
-        y: [],
-      },
-      {
-        xScaleId: "x-1",
-        yScaleId: "y-1",
-        plotter: linePlotter({ style: { strokeStyle: "red" } }),
-        x: new Array(100).fill(0).map((_, i) => i),
-        y: [],
-      },
-    ],
-  },
+  args: {},
 } as ComponentMeta<typeof EmbeddedPlot>;
 
-const Template: ComponentStory<typeof EmbeddedPlot> = ({ ...args }) => (
-  <EmbeddedPlot {...args} />
-);
+const Template: ComponentStory<typeof EmbeddedPlot> = () => {
+  const [data, setData] = useState(() => ({
+    x: [...Array(100).keys()],
+    y: Array(100).fill(0),
+  }));
+
+  const [inputCanvasRef] = usePlot(
+    {
+      dimensions: {
+        height: 400,
+      },
+      plugins: [
+        makeCursorPlugin({
+          onSpanSelect(event) {
+            if (event.phase === "move") {
+              const normalizedStart = Math.min(
+                event.positionStart.scaled["x-1"],
+                event.positionEnd.scaled["x-1"]
+              );
+              const normalizedEnd = Math.max(
+                event.positionEnd.scaled["x-1"],
+                event.positionStart.scaled["x-1"]
+              );
+              setData((data) => ({
+                x: data.x,
+                y: data.y.map((y, i) => {
+                  if (i >= normalizedStart && i <= normalizedEnd) {
+                    return event.positionEnd.scaled["y-1"];
+                  }
+                  return y;
+                }),
+              }));
+            }
+          },
+        }),
+      ],
+    },
+    () => ({
+      padding: 20,
+      axes: [{ scaleId: "x-1" }, { scaleId: "y-1" }],
+      scales: [
+        { id: "x-1" },
+        {
+          id: "y-1",
+          makeLimits: () => ({
+            min: -1,
+            max: 1,
+          }),
+        },
+      ],
+      series: [
+        {
+          xScaleId: "x-1",
+          yScaleId: "y-1",
+          plotter: linePlotter({ style: { strokeStyle: "blue" } }),
+          x: data.x,
+          y: data.y,
+        },
+      ],
+    }),
+    [data]
+  );
+
+  const [outputCanvasRef] = usePlot(
+    {
+      dimensions: {
+        height: 400,
+      },
+      plugins: [makeCursorPlugin({})],
+    },
+    () => ({
+      padding: 20,
+      axes: [{ scaleId: "x-1" }, { scaleId: "y-1" }],
+      scales: [{ id: "x-1" }, { id: "y-1" }],
+      series: [
+        {
+          xScaleId: "x-1",
+          yScaleId: "y-1",
+          plotter: linePlotter({ style: { strokeStyle: "blue" } }),
+          x: data.x,
+          y: data.y.map((y) => Math.sin(y * 2)),
+        },
+      ],
+    }),
+    [data]
+  );
+
+  return (
+    <>
+      <canvas ref={inputCanvasRef} />
+      <canvas ref={outputCanvasRef} />
+    </>
+  );
+};
 
 export const Default = Template.bind({});
