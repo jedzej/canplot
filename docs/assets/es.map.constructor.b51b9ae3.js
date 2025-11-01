a598b305fefe502b1422e8a397eeff5c82c9993c
Jedrzej Wiewiora <jedrzej.wiewiora@gmail.com>
2025-11-01 14:18:12 +0100

    Inset storybook

--- docs/assets/es.map.constructor.b51b9ae3.js
+++
@@ -1,2 +0,0 @@
-var r=Object.defineProperty;var t=(n,o)=>r(n,"name",{value:o,configurable:!0});import{n as e,o as c}from"./iframe.0cbb6995.js";var i=e,l=c;i("Map",function(n){return t(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},l);
-//# sourceMappingURL=es.map.constructor.b51b9ae3.js.map
