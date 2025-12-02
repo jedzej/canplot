bf056ae665108a7a81ae068bd007be7d39206dc3
Jedrzej Wiewiora <jedrzej.wiewiora@gmail.com>
2025-12-02 12:49:39 +0100

    Exporting more

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,47 +2,27 @@
 export { CanPlot } from "./CanPlot";
 
 // Plot components
-export { LinePlot } from "./plot/LinePlot";
-export { ScatterPlot } from "./plot/ScatterPlot";
-export { BarPlot } from "./plot/BarPlot";
-export { AreaPlot } from "./plot/AreaPlot";
-export { SparklinePlot } from "./plot/SparklinePlot";
-export { useXPositioned as usePositioned } from "./plot/Positioned";
+export * from "./plot/LinePlot";
+export * from "./plot/ScatterPlot";
+export * from "./plot/BarPlot";
+export * from "./plot/AreaPlot";
+export * from "./plot/SparklinePlot";
+export * from "./plot/Positioned";
+export * from "./plot/Ticks";
 
 // Interaction components
-export { ChartAreaInteractions } from "./interactions/ChartAreaInteractions";
-export { TooltipsX } from "./interactions/TooltipsX";
-export { Crosshair } from "./interactions/CrossHair";
-export { SelectBox } from "./interactions/SelectBox";
-export { AxisOverlay } from "./interactions/AxisOverlay";
+export * from "./interactions/ChartAreaInteractions";
+export * from "./interactions/TooltipsX";
+export * from "./interactions/CrossHair";
+export * from "./interactions/SelectBox";
+export * from "./interactions/AxisOverlay";
 
 // Types
-export type {
-  PlotScaleConfig,
-  PlotConfiguration,
-  PlotDrawFrame,
-  PlotDrawScaleConfig,
-  PlotPadding,
-  PlotSize,
-  Rect,
-  Style,
-} from "./types";
+export type * from "./types";
 
 // Utilities
-export {
-  valToPos,
-  valToPxDistance,
-  valFits,
-  getScale,
-  isXScale,
-  isYScale,
-  applyStyles,
-  pxToValDistance,
-  clampXPosToChartArea,
-  clampYPosToChartArea,
-  posToVal,
-} from "./helpers";
+export * from "./helpers";
+export * from "./tickUtils";
 
+// Drawing utilities
 export { useDrawEffect, useFrameState } from "./frameContext";
-
-export * from "./tickUtils"
