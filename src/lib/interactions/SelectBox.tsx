01da1f6e81bc60b2892ac9c9163339be5611aea0
Jedrzej Wiewiora <jedrzej.wiewiora@gmail.com>
2025-12-04 15:49:35 +0100

    selectobox

--- src/lib/interactions/SelectBox.tsx
+++ src/lib/interactions/SelectBox.tsx
@@ -7,36 +7,31 @@ export const SelectBox: React.FC<{
   makeClassName?: (selectState: SpanSelectEvent) => string;
   makeStyle?: (selectState: SpanSelectEvent) => React.CSSProperties | undefined;
 }> = ({ makeClassName, makeStyle }) => {
-  const [selectState, setSelectState] = useState<SpanSelectEvent | null>(null);
+  const [selection, setSelectState] = useState<SpanSelectEvent | null>(null);
 
   useInteractionsEvent("spanselect", (event) => {
-    setSelectState(event.mode === "below_threshold" || event.completed ? null : event);
+    setSelectState(
+      event.mode === "below_threshold" || event.completed ? null : event
+    );
   });
 
   const dimensions = useMemo(() => {
-    if (!selectState || selectState.mode === "below_threshold") {
+    if (!selection || selection.mode === "below_threshold") {
       return null;
     }
-    const clampedFromX = clampXPosToChartArea(
-      selectState.frame,
-      selectState.x.css?.from ?? -Infinity,
-      "css"
-    );
-    const clampedToX = clampXPosToChartArea(
-      selectState.frame,
-      selectState.x.css?.to ?? Infinity,
-      "css"
-    );
-    const clampedFromY = clampYPosToChartArea(
-      selectState.frame,
-      selectState.y.css?.from ?? -Infinity,
-      "css"
-    );
-    const clampedToY = clampYPosToChartArea(
-      selectState.frame,
-      selectState.y.css?.to ?? Infinity,
-      "css"
-    );
+    const fromX =
+      selection.mode === "y" ? -Infinity : selection.x.css?.from ?? -Infinity;
+    const toX =
+      selection.mode === "y" ? Infinity : selection.x.css?.to ?? Infinity;
+    const fromY =
+      selection.mode === "x" ? -Infinity : selection.y.css?.from ?? -Infinity;
+    const toY =
+      selection.mode === "x" ? Infinity : selection.y.css?.from ?? Infinity;
+
+    const clampedFromX = clampXPosToChartArea(selection.frame, fromX, "css");
+    const clampedToX = clampXPosToChartArea(selection.frame, toX, "css");
+    const clampedFromY = clampYPosToChartArea(selection.frame, fromY, "css");
+    const clampedToY = clampYPosToChartArea(selection.frame, toY, "css");
 
     const leftPx = Math.min(clampedFromX, clampedToX);
     const topPx = Math.min(clampedFromY, clampedToY);
@@ -44,20 +39,20 @@ export const SelectBox: React.FC<{
     const heightPx = Math.abs(clampedToY - clampedFromY);
 
     return { leftPx, topPx, widthPx, heightPx };
-  }, [selectState]);
+  }, [selection]);
 
   return (
     <div
-      className={selectState ? makeClassName?.(selectState) : undefined}
+      className={selection ? makeClassName?.(selection) : undefined}
       style={{
         position: "absolute",
-        visibility: selectState ? "visible" : "hidden",
+        visibility: selection ? "visible" : "hidden",
         left: `${dimensions?.leftPx ?? 0}px`,
         top: `${dimensions?.topPx ?? 0}px`,
         width: `${dimensions?.widthPx ?? 0}px`,
         height: `${dimensions?.heightPx ?? 0}px`,
         pointerEvents: "none",
-        ...(selectState ? makeStyle?.(selectState) : undefined),
+        ...(selection ? makeStyle?.(selection) : undefined),
       }}
     />
   );
